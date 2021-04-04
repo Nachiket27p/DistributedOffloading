05f62c6ce2691a74f63bd4a4db79e0d403901293
Nachiket Patel <nachiket27p@gmail.com>
2021-04-03 21:49:01 -0400

    rudementary implementation for multithreading

--- code/mainNode.py
+++ code/mainNode.py
@@ -8,13 +8,22 @@ import random as rnd
 from sendReceMatrix import mat_send, mat_receive
 from sendReceMatrix import mat_send_comp, mat_receive_comp
 from sendReceMatrix import DEF_HEADER_SIZE
+from workers import WorkerList
+from threading import Thread, Lock, Semaphore
 
+# worker list
+wList = WorkerList()
+# locks to protect the worker list
+mutex = Lock()
+full = Semaphore(0)
 
 # create logging
 logging.basicConfig(filename='logs/main.log', level=logging.DEBUG)
-logger = logging.getLogger("Main Node: " + str(os.getpid()))
+logger = logging.getLogger("Main:")
 logging.debug("\n\n\n")
 
+# split size
+SPLIT = 4
 # set up socket for this main node
 mainSocket = socket.socket()
 # host = '192.168.1.9'
@@ -23,8 +32,8 @@ port = 5000
 
 ThreadCount = 0
 
-r = 500
-c = 500
+r = 8
+c = 8
 rc = r * c
 # mat_a = np.arange(rc).reshape(r, c)
 # mat_b = np.arange(rc).reshape(r, c)
@@ -50,64 +59,101 @@ logger.info('Waitiing for minimum of ' + str(minWorkers) + ' workers to connect.
 
 
 # used to send work to worker
-def threaded_client(connection, taskID, rows, cols):
+def threaded_client(worker, taskID, rows, cols):
+    tlgr = logging.getLogger(taskID + ':')
     rowsShapeStr = str(rows.shape)
     colsShapeStr = str(cols.shape)
     taskHeader = taskID + '|' + rowsShapeStr + '|' + colsShapeStr
     taskHeaderConf = taskID + '=' + rowsShapeStr + 'x' + colsShapeStr
-    logger.info('Task Header Sent: ' + taskHeader)
+    tlgr.info('Task Header Sent: ' + taskHeader)
 
-    connection.send(str.encode(taskHeader))
+    worker.send(str.encode(taskHeader))
 
-    taskHeaderResponse = (connection.recv(DEF_HEADER_SIZE)).decode('utf-8')
+    taskHeaderResponse = (worker.recv(DEF_HEADER_SIZE)).decode('utf-8')
 
-    logger.info('Task Header Response: ' + taskHeaderResponse)
-    logger.info('Task Header Expected: ' + taskHeaderConf)
+    tlgr.info('Task Header Response: ' + taskHeaderResponse)
+    tlgr.info('Task Header Expected: ' + taskHeaderConf)
 
     if(taskHeaderConf == taskHeaderResponse):
-        # mat_send(connection, rows, logger)
-        mat_send_comp(connection, rows, logger)
-        mat_a_rec = (connection.recv(DEF_HEADER_SIZE)).decode('utf-8')
+        # mat_send(worker, rows, tlgr)
+        mat_send_comp(worker, rows, tlgr)
+        mat_a_rec = (worker.recv(DEF_HEADER_SIZE)).decode('utf-8')
         if(mat_a_rec != rowsShapeStr):
-            logger.info(mat_a_rec)
+            tlgr.info(mat_a_rec)
             return
-        logger.info("Worker Confirmed matrix a received")
+        tlgr.info('Worker Confirmed matrix a received')
 
-        # mat_send(connection, cols, logger)
-        mat_send_comp(connection, cols, logger)
-        mat_b_rec = (connection.recv(DEF_HEADER_SIZE)).decode('utf-8')
+        # mat_send(worker, cols, tlgr)
+        mat_send_comp(worker, cols, tlgr)
+        mat_b_rec = (worker.recv(DEF_HEADER_SIZE)).decode('utf-8')
         if(mat_b_rec != colsShapeStr):
-            logger.info(mat_b_rec)
+            tlgr.info(mat_b_rec)
             return
-        logger.info("Worker Confirmed matrix b received")
+        tlgr.info('Worker Confirmed matrix b received')
 
     else:
-        logger.error("Task not received correctly")
+        tlgr.error('Task not received correctly')
         return
 
-    # results = mat_recieve(connection, logger)
-    results = mat_receive_comp(connection, logger)
+    # results = mat_recieve(worker, tlgr)
+    results = mat_receive_comp(worker, tlgr)
+    # log the results
+    tlgr.info(results)
 
-    logger.info(results)
+    # place the worker back into the free set of the worker list
+    mutex.acquire()
+    wList.freeWorker(worker)
+    mutex.release()
 
 
-# wait for workers to connect
-try:
+def workerHandler():
+    wlgr = logging.getLogger('WorkerHandler:')
     while True:
+        # check if there are enough workers to offload the task
+        # if there are then signal the main thread
+        if wList.freeSize() >= SPLIT:
+            full.release()
+
+        # wait for new workers to join
         worker, address = mainSocket.accept()
-
-        logger.info('Connected to: ' + address[0] + ':' + str(address[1]))
-
-        subTaskID = str(rnd.randint(0, 1024))
-
-        start_new_thread(threaded_client, (worker, subTaskID, mat_a, mat_b))
-
-        ThreadCount += 1
-
-        logger.info('Thread Number: ' + str(ThreadCount))
-
-except KeyboardInterrupt:
-    logger.info('Shutting down server ...')
+        wlgr.info('Connected to: ' + address[0] + ':' + str(address[1]))
+        mutex.acquire()
+        wList.addWorker(worker)
+        mutex.release()
 
 
-mainSocket.close()
+def main():
+    # wait for workers to connect
+    start_new_thread(workerHandler, ())
+
+    try:
+        while True:
+            # try to acquire the semaphore to indicate
+            # enough workers have connected to the
+            full.acquire()
+
+            # acquire the mutex to get access to the worker list
+            mutex.acquire()
+
+            for i in range(SPLIT):
+                start = len(mat_a) // SPLIT * i
+                end = len(mat_a) // SPLIT * (i + 1)
+
+                # get worker
+                worker = wList.getWorker()
+                # assign random id to task which will be used during communication
+                subTaskID = str(rnd.randint(0, 1024))
+                # start a worker thread
+                start_new_thread(threaded_client, (worker, subTaskID, mat_a[start:end, :], mat_b[:, start:end]))
+
+            # release the mutex to free the worker list
+            mutex.release()
+
+    except KeyboardInterrupt:
+        logger.info('Shutting down server ...')
+
+    mainSocket.close()
+
+
+if __name__ == '__main__':
+    main()
